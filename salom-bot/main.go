b22689d359a5e87335429ec61ad727a60e368582
Turgunov Mansurali <turgunovmansurali19@gmail.com>
2025-11-16 21:46:21 +0500

    terminalda yozgan ismga salom beruvchi bot

---
+++ salom-bot/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func salom_bot() {
+	var name string
+
+	fmt.Println("ismingizni kiriting:")
+	fmt.Scanln(&name)
+	fmt.Println("salom", name, "!")
+
+}
+
+//terminalga yozgan ismingizga salom beradi
